ebac8a17cde53ece7d6e57794cfddcdbb9df15ac
Artur Valeev <art.valeev@dodobrands.io>
2025-05-29 16:34:30 +0300

    hide bytes input

--- src/renderer/qr.ts
+++ src/renderer/qr.ts
@@ -7,7 +7,7 @@ const MIN_SCAN_LENGTH = 5; // Минимальная длина последов
 
 let isScanning = false;
 let rawString: string = "";
-let rawBytes: number[] = [];
+// let rawBytes: number[] = [];
 let lastKeyPressTime = 0;
 let inputTimeout: NodeJS.Timeout | null = null;
 
@@ -25,7 +25,7 @@ export const useBinaryScanner = (onScan: (data: string) => void): void => {
         // Если задержка больше порога, сбрасываем сканирование
         isScanning = false;
         rawString = '';
-        rawBytes = [];
+        // rawBytes = [];
       }
 
       lastKeyPressTime = currentTime;
@@ -36,22 +36,22 @@ export const useBinaryScanner = (onScan: (data: string) => void): void => {
       }
       inputTimeout = setTimeout(() => {
         if (isScanning && rawString.length >= MIN_SCAN_LENGTH) {
-          const text = new TextDecoder("utf-8").decode(new Uint8Array(rawBytes));
-          console.log('text from bytes', text);
+          // const text = new TextDecoder("utf-8").decode(new Uint8Array(rawBytes));
+          // console.log('text from bytes', text);
           
           onScan(rawString);
         }
 
         isScanning = false;
         rawString = '';
-        rawBytes = [];
+        // rawBytes = [];
       }, SCAN_COMPLETE_DELAY_MS);
 
       // Начало сканирования (первый символ)
       if (!isScanning && char.length === 1) {
         isScanning = true;
         rawString = char;
-        rawBytes = [char.charCodeAt(0)];
+        // rawBytes = [char.charCodeAt(0)];
         return;
       }
 
@@ -59,7 +59,7 @@ export const useBinaryScanner = (onScan: (data: string) => void): void => {
       if (isScanning) {
         if (char.length === 1) {
           rawString += char;
-          rawBytes.push(char.charCodeAt(0));
+          // rawBytes.push(char.charCodeAt(0));
         }
       }
     };
